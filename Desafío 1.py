99a534940eef1815ac7d4b9b126bc19fa68cc622
ruben-samurio <rubensamurio97@gmail.com>
2025-04-06 16:54:50 -0300

    Add files via upload

---
+++ Desafío 1.py
@@ -0,0 +1,4 @@
+numero1 = 5
+numero2 = 7
+resultado = numero1 + numero2
+print ("la suma de", numero1, "y", numero2, "es:", resultado)
---
+++ Desafío 2.py
@@ -0,0 +1,11 @@
+Título = "Cálculo del área de un rectángulo"
+Descripción = "Este programa calcula el área de un rectánculo usando valores predefinidos de ancho y largo"
+Ancho = 5#metros
+Largo = 10#metros
+Área= Ancho * Largo
+print ("Cálculo del área de un rectángulo")
+print ("="*len (Título))
+print ("Este programa calcula el área de un rectángulo usando valores predefinidos de ancho y largo")
+print (f"\n el ancho del rectángulo es: {Ancho} metros")
+print (f" el largo del rectángulo es: {Largo} metros")
+print (f" Por lo tanto, el área del rectángulo es: {Área} metros cuadrados")
